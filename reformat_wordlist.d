cd139d1bc96139083ddb7fafcb708063eff5f325
Gautam Kotian <gautam.kotian@gmail.com>
2015-06-25 00:00:27 +0200

    Improvements to reformatting script

--- reformat_wordlist.d
+++ reformat_wordlist.d
@@ -1,4 +1,3 @@
-import std.conv;
 import std.stdio;
 import std.string;
 
@@ -8,33 +7,84 @@ int main()
     int num_columns = 3;
     int number_word_gap = 2;
     int column_gap = 8;
+    int chars_per_row = 100;
+
+    int chars_per_column = chars_per_row / num_columns;
+
+    int number_width = 5 + number_word_gap;
+    int word_width = chars_per_column - number_width;
+
+    auto entries_per_page = num_rows * num_columns;
 
     File fd = File("English/wordlist.txt", "r");
 
     struct Pair
     {
-        int number;
+        string number;
         string word;
     }
 
     Pair[] page_contents;
 
-    page_contents.length = num_rows * num_columns;
+    page_contents.length = entries_per_page;
 
-    int i = 0;
+    int page_num = 0;
 
-    for (i = 0; i < (num_rows * num_columns); ++i)
+    while (!fd.eof())
     {
-        if (fd.eof())
+        int i;
+
+        for (i = 0; (i < entries_per_page) && !fd.eof(); ++i)
         {
-            break;
+            string line = strip(squeeze(detab(chomp(fd.readln()), 1), " "));
+
+            if ( line.length == 0 )
+            {
+                --i;
+                continue;
+            }
+
+            page_contents[i].number = line[0..5];
+            page_contents[i].word = line[6..$];
         }
 
-        string line = squeeze(detab(chomp(fd.readln()), 1), " ");
+        // writefln("i = %d, Page %d : '%s' to '%s'", i, page_num++,
+        //     page_contents[0], page_contents[i-1]);
+        auto entries_in_cur_page = i - 1;
 
-        page_contents[i].number = to!int(line[0..5]);
-        page_contents[i].word = line[6..$];
-        writefln("%d -> %s", page_contents[i].number, page_contents[i].word);
+        int entries_shown = 0;
+
+        for (auto row = 0; row < num_rows; ++row)
+        {
+            int index_of_entry;
+            int col;
+
+            for (col = 0; col < num_columns - 1; ++col)
+            {
+                index_of_entry = (col * num_rows) + row;
+
+                writef("%s%s",
+                    leftJustify(page_contents[index_of_entry].number, number_width),
+                    leftJustify(page_contents[index_of_entry].word, word_width));
+
+                ++entries_shown;
+            }
+
+            index_of_entry = (col * num_rows) + row;
+
+            writef("%s%s\n",
+                leftJustify(page_contents[index_of_entry].number, number_width),
+                page_contents[index_of_entry].word);
+
+            ++entries_shown;
+
+            if ( entries_shown == entries_in_cur_page )
+            {
+                break;
+            }
+        }
+
+        writeln();
     }
 
     fd.close();
